979569eccd2a28f7e4e694bc597808aea46e2892
Kevin Wang <kevinwang@kevinde-mbp.attlocal.net>
2023-03-10 07:09:32 -0800

    Update

--- main.py
+++ main.py
@@ -6,17 +6,59 @@ from flask_restful import Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
-class RobotServer(Resource):
+
+robots = [
+    {
+        'filename': 'testRobot1',
+        'name': 'robot1',
+        'isRobot': True
+    },
+    {
+        'filename': "testRobot2",
+        'name': 'robot2',
+        'isRobot': True
+    }
+]
+
+robot_attributes = {
+    'filename': fields.String,
+    'name': fields.String,
+  	"isRobot": fields.boolean,
+}
+
+class RobotList():
 	def get(self):
-		return {"data": "robot 1"}
+		return  {'robot': [marshal(robot, robot_attributes) for robot in robots]}
 
-	def get(self, name):
-		return {"data": name}
+    def post(self):
+        args = self.reqparse.parse_args()
+        robot = {
+            'filename': args['filename'],
+            'name': args['name'],
+            'isRobot': args['isRobot'],
+        }
+        robots.append(robot)
+        return {'robot': marshal(robot, robot_attributes)}, 201
 
-	def post(self):
+
+class Robot(Resource):
+	def get(self, filename):
+        robot = [robot for robot in robots if robot['filename'] == filename]
+        if len(robot) == 0:
+            abort(404, message="Cannot find this robot $filename")
+        return {'robot': marshal(robot[0], robot_attributes)}
+
+
+	def put(self, filename):
 		return {"data": "Posted"}
 
-api.add_resource(RobotServer, "/api/robot/<string:name>")
+	def delete(self, filename):
+		return
+
+api.add_resource(RobotList, '/api/robot', endpoint='robots')
+api.add_resource(Robot, '/api/robot/<string:filename>', endpoint='robot')
+
+
 
 if __name__ == "__main__":
 	app.run(debug=True)

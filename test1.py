c2edea595826452950d4ed31fea00832d098181a
Kevin Wang <kevinwang@kevinde-mbp.attlocal.net>
2023-03-10 04:07:06 -0800

    test update

--- test1.py
+++ test1.py
@@ -1,8 +1,8 @@
 #! /usr/bin/env python3
 
-import requests
+import urllib. requests
 
-BASE = "https://127.0.0.1:5000/"
+BASE = "http://127.0.0.1:5000/"
 
 response = requests.get(BASE + "api")
 print(response.json())

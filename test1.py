040fc18101fb5cf9d603ba2ef56bed1c25fe0a86
Kevin Wang <kevinwang@kevinde-mbp.attlocal.net>
2023-03-10 08:25:38 -0800

    Added tests

--- test1.py
+++ test1.py
@@ -2,6 +2,8 @@
 
 import requests
 
+#Test the first case of GET all properties
+
 BASE = "http://127.0.0.1:5000/"
 
 response = requests.get(BASE + "api/robot")
---
+++ test2.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python3
+
+import requests
+
+#Test the second case of GET a specific robot
+
+BASE = "http://127.0.0.1:5000/"
+
+response = requests.get(BASE + "api/robot/puma.json")
+print(response.json())

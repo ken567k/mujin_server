2d1e383fb13e4794c2b1fc7953209aed09cd30fb
Kevin Wang <kevinwang@kevinde-mbp.attlocal.net>
2023-03-10 08:36:53 -0800

    Added test

---
+++ test3.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python3
+
+import requests
+
+#Test the third case of GET a download of specific robot
+
+BASE = "http://127.0.0.1:5000/"
+
+response = requests.get(BASE + "api/robot/puma.json/True")
+print(response.json())
